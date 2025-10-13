d5b90cb8079c1c7a78d8bf0701b78f29f533a785
Mandeep Ghanghas <mk423041@gmail.com>
2025-10-13 22:46:46 +0530

    cookies and sessions

---
+++ Ch18-Cookies And Sessions/controllers/authController.js
@@ -0,0 +1,23 @@
+exports.getLogin = (req, res, next) => {
+    res.render("auth/login", {
+        pageTitle: "Login",
+        currentPage: "login",
+        isLoggedIn: false
+    });
+}
+
+exports.postLogin = (req, res, next) => {
+    console.log(req.body);
+
+    req.session.isLoggedIn = true;
+
+    // res.session("isLoggedIn",true);
+    // req.isLoggedIn = true;
+    res.redirect("/");
+}
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(() => {
+        res.redirect("/login");
+    });
+}
